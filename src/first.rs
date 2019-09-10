fdd914647c822d98b9b29eb23f8d65f1b6aaa082
Brigette Valdez <valdez.brigette@gmail.com>
2019-09-10 12:38:44 -0500

    Update first.rs
    added testing and drop for the linked list

--- src/first.rs
+++ src/first.rs
@@ -1,3 +1,4 @@
+
 use std::mem;
 
 pub struct List {
@@ -19,8 +20,7 @@ impl List {
         List { head: Link::Empty }
     }
 
-    //pushing a new value to the list
-    pub fn push (&mut self, elem: i32) {
+    pub fn push(&mut self, elem: i32) {
         let new_node = Box::new(Node {
             elem: elem,
             next: mem::replace(&mut self.head, Link::Empty),
@@ -29,7 +29,7 @@ impl List {
         self.head = Link::More(new_node);
     }
 
-    /*pub fn pop(&mut self) -> Option<i32> {
+/*pub fn pop(&mut self) -> Option<i32> {
         let result;
         match mem::replace(&mut self.head, Link::Empty) {
             Link::Empty => {
@@ -47,15 +47,56 @@ impl List {
      *this is simply a shorter way of writing it 
      */
     pub fn pop(&mut self) -> Option<i32> {
-    match mem::replace(&mut self.head, Link::Empty) {
-        Link::Empty => None,
-        Link::More(node) => {
-            self.head = node.next;
-            Some(node.elem)
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
         }
     }
 }
 
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
 
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
 }
 
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
